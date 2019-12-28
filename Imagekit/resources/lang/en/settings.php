ff89d573dfd4192e3b206f9b4b112613fea20a21
Michael Aerni <hello@michaelaerni.ch>
2019-12-28 10:10:07 +0100

    Add addon folder with new name

---
+++ Imagekit/resources/lang/en/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+  'domain' => 'Domain',
+  'domain_instruct' => 'Domain used for the URL Endpoints.',
+  'id' => 'ImageKit ID',
+  'id_instruct' => 'This is only required for the free plan of ImageKit.',
+  'identifier' => 'Endpoint Identifier',
+  'identifier_instruct' => 'An optional default identifier for your URL Endpoints.',
+];
